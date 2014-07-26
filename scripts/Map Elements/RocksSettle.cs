db4d23cc40bfdf077c62897d7cf6a6b6d69498e9
Sander <sandermoolin@gmail.com>
2014-07-26 14:35:53 -0400

    Gravity, level balancing

---
+++ scripts/Map Elements/RocksSettle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocksSettle : MonoBehaviour {
+
+	public void Settle()
+	{
+		GetComponent<PolygonCollider2D>().enabled = true;
+		gameObject.AddComponent<Rigidbody2D>();
+		transform.parent = null;
+		gameObject.layer = 8;
+	}
+}
