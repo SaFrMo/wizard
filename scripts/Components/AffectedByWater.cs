578125ba99ed01a95b32b6eadbe83b42fde3cbb0
Sander <sandermoolin@gmail.com>
2014-07-29 18:55:18 -0400

    Test pre-cherry-picking

--- scripts/Components/AffectedByWater.cs
+++ scripts/Components/AffectedByWater.cs
@@ -5,22 +5,14 @@ public class AffectedByWater : MonoBehaviour {
 
 	public enum Effect
 	{
-		NeedsWaterToSurvive,
-		Floats
+		NeedsWaterToSurvive
 	}
 	
 	public Effect effect = Effect.NeedsWaterToSurvive;
 
-	private SpringJoint2D spring = null;
 
-	private Vector2 targetPosition;
 
-	private void LockNewPosition ()
-	{
-		rigidbody2D.MovePosition(new Vector2(transform.position.x,
-		                              	 GetComponent<GeneralInfo>().water.renderer.bounds.max.y));
-		rigidbody2D.gravityScale = 0;
-	}
+
 	
 	public void ApplyEffect (Vector2 source) 
 	{
@@ -29,53 +21,18 @@ public class AffectedByWater : MonoBehaviour {
 			if (!GetComponent<GeneralInfo>().isInWater)
 				GetComponentInParent<GeneralInfo>().Kill();
 			break;
-		case Effect.Floats:
-			if (spring == null && GetComponent<GeneralInfo>().isInWater)
-			{
-				spring = gameObject.AddComponent<SpringJoint2D>();
-				spring.anchor = (Vector2)GetComponent<BoxCollider2D>().bounds.center.normalized;
-				spring.connectedAnchor = new Vector2 (transform.position.x,
-				                             		  GetComponent<GeneralInfo>().water.renderer.bounds.max.y);
-				spring.distance = .1f;
-				spring.dampingRatio = 10f;
-				spring.frequency = 10f;
-				rigidbody2D.gravityScale = .1f;
-			}
-
-			else if (spring != null)
-			{
-				spring.anchor = (Vector2)GetComponent<BoxCollider2D>().bounds.center.normalized;
-				spring.connectedAnchor = new Vector2 (transform.position.x,
-				                                      GetComponent<GeneralInfo>().water.renderer.bounds.max.y);
-				spring.distance = .1f;
-			}
-
-			/*
-			if (GetComponent<GeneralInfo>().isInWater && GetComponent<GeneralInfo>().water.renderer.bounds.max.y - transform.position.y > 2f)
-			{
-
-				// the transform.position must equal the top of the water
-				iTween.MoveTo (gameObject, new Hashtable() {
-					{ iT.MoveTo.y, GetComponent<GeneralInfo>().water.renderer.bounds.max.y },
-					{ iT.MoveTo.speed, 2f },
-					{ iT.MoveTo.easetype, iTween.EaseType.easeOutElastic },
-					{ iT.MoveTo.oncomplete, "LockNewPosition" }
-				});
-
-			}
-			*/
-			break;
 		}
 	}
 
 	private void Start ()
 	{
 		Invoke ("Check", .5f);
+
 	}
 
 	private void Check ()
 	{
-		if (effect == Effect.NeedsWaterToSurvive || effect == Effect.Floats)
+		if (effect == Effect.NeedsWaterToSurvive)
 			StartCoroutine("CheckForWater");
 	}
 
@@ -84,7 +41,7 @@ public class AffectedByWater : MonoBehaviour {
 		for (;;)
 		{
 			ApplyEffect(Vector2.zero);
-			yield return new WaitForSeconds(.4f);
+			yield return new WaitForSeconds(2f);
 		}
 	}
 }
--- scripts/Map Elements/RocksSettle.cs
+++ scripts/Map Elements/RocksSettle.cs
@@ -9,22 +9,5 @@ public class RocksSettle : MonoBehaviour {
 		gameObject.AddComponent<Rigidbody2D>();
 		transform.parent = null;
 		gameObject.layer = 8;
-		s = GetComponent<SpriteRenderer>();
-		StartCoroutine ("FadeAway");
-	}
-
-	SpriteRenderer s;
-
-	private IEnumerator FadeAway()
-	{
-		for (int i = 100; i > 0; i--)
-		{
-			s.color = new Color(s.color.r,
-			                    s.color.g,
-			                    s.color.b,
-			                    i / 100f);
-			yield return null;
-
-		}
 	}
 }
